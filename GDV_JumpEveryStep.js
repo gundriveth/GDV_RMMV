935b0ac107b03cb710c6412a65d02873683e24a9
Gundriveth <billy.santoso93@gmail.com>
2018-04-12 17:55:06 +0700

    Add files via upload

---
+++ GDV_JumpEveryStep.js
@@ -0,0 +1,41 @@
+//=============================================================================
+// PluginName.js
+//=============================================================================
+/*:
+ * @plugindesc Description of your plugin
+ *
+ * @author Gundriveth
+ *
+ * @help
+ *
+ * Anything users might need to know about using your plugin.
+ *
+ * TERMS OF USE
+ * You can use the plugin in your project. Don't forget to credit me :)
+ *
+ * COMPATIBILITY
+ * Any compatibility issues you know of or that have been brought to your attention, such as not being able to use this plugin with one of Galv's for example. This will be most common when you have a plugin that does the same or a similar thing to someone else's.
+ */
+ 
+(function() {
+    
+    var _Game_Player_executemove = Game_Player.prototype.executeMove;
+    var _Game_Player_jump = Game_Player.prototype.jump;
+    var xPlus = 0;
+    var yPlus = 0;
+
+    Game_Player.prototype.executeMove = function(direction) {
+
+        _Game_Player_executemove.call(this, direction);
+        _Game_Player_jump.call(this, xPlus, yPlus);
+    };
+
+
+
+ //   Game_Player.prototype.jump = function(xPlus, yPlus) {
+ //       Game_Character.prototype.jump.call(this, xPlus, yPlus);
+ //       this._followers.jumpAll();
+ //   };
+
+
+})();
